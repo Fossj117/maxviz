1174e1ff2658fb81ad885d1dc37b2dc95ff31bdd
Jeffrey Fossett <jeffreyfossett@Jeffreys-MacBook-Pro-5.local>
2019-06-12 16:29:15 -0400

    add logic for coloring different node distances

--- process_network_data.py
+++ process_network_data.py
@@ -3,8 +3,10 @@ import networkx as nx
 import json
 import os
 
-def links_data_to_dot_file(infile="./network_data_raw/Max_Network_Data_Clean_Links.csv", outfile='./network.dot'):
-	'''Read in the network links data and write the network structure to a dot file''' 
+def build_graph_from_links_data(infile = "./network_data_raw/Max_Network_Data_Clean_Links.csv"):
+	'''
+	Read from file and return NetworkX graph object
+	'''
 
 	G = nx.DiGraph()
 	links = pd.read_csv(infile)
@@ -13,9 +15,19 @@ def links_data_to_dot_file(infile="./network_data_raw/Max_Network_Data_Clean_Lin
 
 		G.add_edge(link['Parent_First_Last'].decode('utf-8'), link['Child_First_Last'].decode('utf-8'))
 
-	nx.write_dot(G, 'temp.txt')
+	return G
 
-	with open('temp.txt', "r") as f:
+
+def links_data_to_dot_file(infile="./network_data_raw/Max_Network_Data_Clean_Links.csv", outfile='./network.dot'):
+	'''
+	Read in the network links data and write the network structure to a dot file
+	''' 
+
+	G = build_graph_from_links_data(infile)
+
+	nx.write_dot(G, 'raw_network.dot')
+
+	with open('raw_network.dot', "r") as f:
 		with open(outfile, 'w') as out: 
 
 			for ix, line in enumerate(f): 
@@ -25,19 +37,31 @@ def links_data_to_dot_file(infile="./network_data_raw/Max_Network_Data_Clean_Lin
 					out.write(line.strip())
 			out.write("'")
 
-	os.remove('temp.txt')
+	os.remove('raw_network.dot')
+
 
 def node_metadata_to_file(infile="./network_data_raw/Max_Network_Data_Clean_People.csv", outfile='./node_data.json'):
-	'''Read in the node metadata and write relevant pieces to a JSON'''
+	'''
+	Read in the node metadata and write relevant pieces to a JSON. 
+	This also does the graph processing to find the steps for each node 
+	'''
 
+	# Build and initialize things
 	nodes = pd.read_csv(infile)
+	G = build_graph_from_links_data() # default reads the links
 	kv = {}
 
+	# Add websites to node metadata
 	for _, node in nodes[['First_Last', 'Website']].iterrows(): 
 
-		# Just adding their website for now if it exists 
+		node_key = node['First_Last']
+
+		# Compute distance from Max as an integer and store it
+		kv[node_key] = {'steps': nx.shortest_path_length(G, source='Max Bazerman', target = node_key.decode('utf8'))}
+
+		# Add website if it exists 
 		if not pd.isnull(node['Website']): 
-			kv[node['First_Last']] = {'site': node['Website']}
+			kv[node_key]['site'] = node['Website']
 
 	# Write to file with some hacks to later load with JS
 	with open(outfile, "w") as f:
